4105dcd457e7ed844bf1a6bc89c661ae5ec03507
muromtsev <t4k.org@mail.ru>
2024-06-21 20:43:50 +0300

    Set

---
+++ src/holding/setOfInteger.java
@@ -0,0 +1,14 @@
+package holding;
+
+import java.util.*;
+
+public class setOfInteger {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        Set<Integer> intset = new HashSet<>();
+        for (int i = 0; i < 10000; i++) {
+            intset.add(rand.nextInt(30));
+        }
+        System.out.println(intset);
+    }
+}
